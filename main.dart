57ff6cf96bf43dcdc7a9fc08c553d75ce137dd6e
raju-islam <rajuslam39@gmail.com>
2023-11-05 22:51:17 +0600

    ducument in Function

---
+++ main.dart
@@ -0,0 +1,62 @@
+/*void main() {
+  myfun();
+}
+//Calling a Funtion
+void myfun() {
+  var a = 5;
+  var b = 3;
+  print(a + b);
+}*/
+/*void main() {
+  print(myfun());
+}
+//Return int in Funtion
+int myfun() {
+  var a = 10;
+  var b = 10;
+  var c = a + b;
+  return c;
+}*/
+/*void main()
+{
+print(myfun());
+}
+//Return String in Funtion
+String myfun(){
+  var name="Raju";
+  return name;
+}*/
+/*void main(){
+  print(myfun());
+}
+//Return List in Funtion
+List myfun(){
+  var list =[10,20,"Raju"];
+  return list;
+}*/
+/*import 'dart:ui_web';
+
+void main(){
+
+  print(myfun());
+}
+//Return Map in Funtion
+Map myfun(){
+  var mylist={
+    "name":"Raju","roll":10,"college":"Janata college"
+  };
+  return mylist;
+}
+*/
+
+
+
+void main(){
+myfun(30, 20);
+}
+// Parameterized Function (argument)
+void myfun(var a, var b){
+  print(a+b);
+}
+
+
